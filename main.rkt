9e57de4b6341673ed4537c7bab30a3e22cc95c81
Moe Kayali <moe@kayali.io>
2020-04-09 19:51:24 -0700

    spacing and comments

--- main.rkt
+++ main.rkt
@@ -4,26 +4,6 @@
     (require rackunit/text-ui
         "test/lang.rkt"))
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
-
-;; Code here
-
-
 (module+ test
     ;; Any code in this `test` submodule runs when this file is run using DrRacket
     ;; or with `raco test`. The code here does not run when this file is
