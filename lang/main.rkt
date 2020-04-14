1c56120f5256e4e612942ea42e183bb9132d2c72
Moe Kayali <moe@kayali.io>
2020-04-14 15:46:03 -0700

    add contract for `create-model`

--- lang/main.rkt
+++ lang/main.rkt
@@ -1,13 +1,17 @@
 #lang racket/base
 
-(require "reader.rkt"
+(require racket/contract
+	     "reader.rkt"
          "expander.rkt"
          "grounding.rkt")
+
+(provide (contract-out
+	 	  [create-model (input-port? . -> . (listof rule?))]))
+
+(provide load-data)
 
 (define (create-model port)
     (let* ([ast (load-rules port)]
            [model (handle-model ast)])
         model))
-(provide create-model)
 
-(provide load-data)
