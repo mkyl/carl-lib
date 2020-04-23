bab61698a9e906b0b91da1024e8c3fae32a1e25b
Moe Kayali <moe@kayali.io>
2020-04-23 14:46:58 -0700

    fix undeclared dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang info
 (define collection "carl-lib")
-(define deps '("base" "brag-lib" "db" "graph"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "brag-lib" "db" "graph" "math-lib" "rackunit-lib"
+	"scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "math-doc"))
 (define scribblings '(("scribblings/carl-lib.scrbl" ())))
 (define pkg-desc "Causal Relational Learning library")
 (define version "0.0.1")
