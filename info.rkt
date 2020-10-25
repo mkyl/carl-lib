5b34a102918e0da7355bffff55d4000b15b5a5fc
Moe Kayali <moe@kayali.io>
2020-10-24 22:41:42 -0700

    bump up to v0.1.0

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "math-doc"))
 (define scribblings '(("scribblings/carl-lib.scrbl" ())))
 (define pkg-desc "Causal Relational Learning library")
-(define version "0.0.1")
+(define version "0.1.0")
 (define pkg-authors '(Moe Kayali))
