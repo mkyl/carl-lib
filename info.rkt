16cd63fc3cc8d4cb6a74277552e6def3698badde
Moe Kayali <moe@kayali.io>
2020-10-24 22:38:32 -0700

    add dependency on graphviz

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "carl-lib")
 (define deps '("base" "brag-lib" "db" "graph" "math-lib" "rackunit-lib"
-	"scribble-lib" "csv-writing"))
+	"scribble-lib" "csv-writing" "graphviz"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "math-doc"))
 (define scribblings '(("scribblings/carl-lib.scrbl" ())))
 (define pkg-desc "Causal Relational Learning library")
